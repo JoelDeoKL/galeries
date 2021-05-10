f46a618ea2565580c9e0fde4967ec45fc56fd91a
JoelDeoKL <joeldeocm88@gmail.com>
2021-05-10 11:18:35 +0200

    Ajout d'un controlleur qui enregistrera les elements dans la base de donnees

---
+++ index.php
@@ -0,0 +1,43 @@
+<!-- Système d'upload d'image sans utiliser une base de données -->
+<!-- Bootstrap 4 -- PHP -->
+
+<!-- upload.php se charge recuperer les images sur le serveur et le copie à la racine
+du projet. -->
+
+<!-- Par JoelDeoKL -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-4 col-2">
+                <div class="form-group">
+                    <hr>
+                    <label>Creer un dossier</label>
+                    <input type="text" name="auteur" id="auteur"  class="form-control">
+                </div>
+                <div class="col-2">
+                    <input type="submit" value="Ajouter" name="ajouter_citation" class="btn btn-success float-right" name="inscrire">
+                </div>
+                <br><br>
+                <div class="form-control">
+                    <input type="hidden" name="MAX_FILE_SIZE" value="100000" />
+                    <input type="file" name="img" id="img">
+                    <input type="submit" value="uploader">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="bootstrap/js/jquery.min.js"></script>
+    <script src="bootstrap/js/jquery.min.js"></script>
+</body>
+</html>
